bf732aaaef360bd4e6ae4aed05da1a43d53cbe60
Estevao Pereira <estevao.pereira@codenapp.com>
2023-05-04 14:01:26 -0300

    aaa

---
+++ personal_finances_next_js/src/pages/revenueCategory/index.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import  Sidebar  from '../../components/Sidebar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Page() {
+  return (
+    <main
+      className={`flex`}
+    >
+      <div>
+        <Sidebar/>
+      </div>
+      <div><h1>OOOOOOOOOOOI</h1></div>
+    </main>
+  )
+}
